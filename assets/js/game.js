9b1f2f1434838c9e9fa4d630b829b2e8f0319c22
Mike Veneziale <falciformdesign@gmail.com>
2021-07-07 12:05:47 -0500

    loop through multiple robot names

--- assets/js/game.js
+++ assets/js/game.js
@@ -7,11 +7,13 @@ var playerMoney = 10;
 //log multiple values at once like this
 console.log(playerName, playerAttack, playerHealth);
 
-var enemyName = "Roborto";
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 10;
 
-var fight = function() {
+
+
+var fight = function(enemyName) {
     // Alerts players that they are starting th round
     window.alert("Welcome to Robot Gladiators!");
 
@@ -53,7 +55,7 @@ var fight = function() {
         if (confirmSkip) {
             window.alert(playerName + " has decided to skip the fight");
             playerMoney = playerMoney - 2;
-            console.log(playerMoney - 2);
+
         }
         else {
             fight();
@@ -64,12 +66,12 @@ var fight = function() {
         window.alert("You need to choose a valid option, try again");
     }
 
-
-
-
 };
 
+for(var i = 0; i < enemyNames.length; i++) {
+    fight(enemyNames[i]);
+}
 
 
-
-fight()
+// Executes the Fight function
+// fight()
